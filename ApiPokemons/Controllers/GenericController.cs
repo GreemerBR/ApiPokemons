b44aa2ae9221793f505e1943ae65ab051ebc1dd0
Gregory Viegas Zimmer <gregory.v.zimmer@gmail.com>
2022-08-29 13:34:45 -0300

    Criado o repositório genérico

---
+++ ApiPokemons/Controllers/GenericController.cs
@@ -0,0 +1,46 @@
+﻿using Data.Model;
+using Data.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPokemons.Controllers
+{
+    public class GenericController<T> : ControllerBase where T : BaseModel
+    {
+        private BaseRepository<T> _repository;
+
+        public GenericController()
+        {
+            this._repository = new BaseRepository<T>();
+        }
+
+        [HttpGet]
+        public List<T> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        [HttpPost]
+        public string Post(T model)
+        {
+            return _repository.Create(model);
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return _repository.Delete(id);
+        }
+
+        [HttpPut]
+        public string Update(T model)
+        {
+            return _repository.Update(model);
+        }
+    }
+}
--- ApiPokemons/Controllers/PokemonController.cs
+++ ApiPokemons/Controllers/PokemonController.cs
@@ -6,43 +6,7 @@ namespace ApiPokemons.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PokemonController : ControllerBase
+    public class PokemonController : GenericController<Pokemon>
     {
-        private PokemonRepository _repository;
-
-        public PokemonController()
-        {
-            this._repository = new PokemonRepository();
-        }
-
-        [HttpGet(Name = "Pokemon")]
-        public List<Pokemon> GetAll()
-        {
-            return _repository.GetAll();
-        }
-
-        [HttpGet("{id}")]
-        public Pokemon GetById(int id)
-        {
-            return _repository.GetById(id);
-        }
-
-        [HttpPost]
-        public string Post(Pokemon model)
-        {
-            return _repository.Create(model);
-        }
-
-        [HttpDelete("{id}")]
-        public string Delete(int id)
-        {
-            return _repository.Delete(id);
-        }
-
-        [HttpPut]
-        public string Update(Pokemon model)
-        {
-            return _repository.Update(model);
-        }
     }
 }
